60d510d76beecebe174dd0952628c7ea01ea469f
umepy <haruya04183083@gmail.com>
2022-07-06 14:26:31 +0000

    feat: add num str tuple array lines

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,4 +1,22 @@
 fn main() {
-    let mut x = 5;
-    println!(x);
+    // Numerical types
+    let i1 = 5;
+    let i2: i8 = 10;
+
+    // String types
+    let s1: String = String::from("Hello");
+    let s2: &str = &s1;
+    let s3: String = s2.to_string();
+
+    // Tuples
+    let mut tup = (1, "2");
+    tup.0 = 10;
+    tup.1 = "20";
+
+    // Arrays
+    let mut arr = [0, 1, 2];
+    let arr2 = [0; 10];
+    for i in 0..arr2.len() {
+        println!("{}", arr2[i]);
+    }
 }
